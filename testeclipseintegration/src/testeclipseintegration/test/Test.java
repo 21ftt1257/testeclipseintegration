ec54fa7ee9aa7eb9e57d51f54d7dea15b51f612b
Zulfiqry Zulkarnaen <21FTT1257@student.pb.edu.bn>
2023-01-31 15:28:01 +0800

    Trying basic OOP

--- testeclipseintegration/src/testeclipseintegration/test/Test.java
+++ testeclipseintegration/src/testeclipseintegration/test/Test.java
@@ -1,5 +1,13 @@
 package testeclipseintegration.test;
 
 public class Test {
+	public static void main(String[] args) {
 
+		System.out.println("GitHub");
+		
+		
+		ClassA classA = new ClassA("ABCDEF");
+		
+		System.out.println(classA.getAbc());
+	}
 }
